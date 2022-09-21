4856b19d262f0159b2ab311711898f7d7d5c74a7
eborchard <eborchard@uiowa.edu>
2022-09-21 14:11:32 -0500

    JUnit test class for WorkLoad created.

---
+++ src/edu/uiowa/cs/warp/WorkLoadTest.java
@@ -0,0 +1,59 @@
+package edu.uiowa.cs.warp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WorkLoadTest {
+
+	@Test
+	void testGetMinPeriod() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFlowNamesInOriginalOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFlowNamesInPriorityOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNodeChannel() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFlowDeadline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFlowsInPriorityOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetFlowsInDMorder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNodeNamesOrderedAlphabetically() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNodesInFlow() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMaxFlowLength() {
+		fail("Not yet implemented");
+	}
+
+}
