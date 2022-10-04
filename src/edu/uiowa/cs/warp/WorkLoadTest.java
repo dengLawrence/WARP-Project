4f58caccbf8c22b4f16564f651334a88797867fe
eborchard <eborchard@uiowa.edu>
2022-10-04 16:06:34 -0500

    Broke up larger tests into individual test cases, and made error messages more clear in WorkLoadTest.java

--- src/edu/uiowa/cs/warp/WorkLoadTest.java
+++ src/edu/uiowa/cs/warp/WorkLoadTest.java
@@ -13,7 +13,7 @@ class WorkLoadTest {
 	 * Begin setFlowsInDMorder tests
 	 * ------------------------------ */
 	@Test
-	void testSetFlowsInDMorder() {
+	void testSetFlowsInDMorder_StressTest() {
 		//instantiation of a new WorkLoad object using StressTest.txt as test file
 		WorkLoad wkld = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
 		//create ArrayList of flows in their original order
@@ -27,7 +27,7 @@ class WorkLoadTest {
 		//find length of DM order flow array
 		int DMorderLength = DMorder.size();
 		//check to see if the two lengths are equivalent after method call
-		assertEquals(origOrderLength, DMorderLength, "length after setting flows in DM Order:");
+		assertEquals(origOrderLength, DMorderLength, "Length after setting flows in DM Order:");
 		
 		//create a counter and loop to check if every element in the original array list is in the DM order array list
 		int containsCntr = 0;
@@ -36,21 +36,20 @@ class WorkLoadTest {
 				containsCntr++;
 		}
 		//check to see if the counter matches the length of the original array list
-		assertEquals(origOrderLength, containsCntr, "number of equivalent flows:");
+		assertEquals(origOrderLength, containsCntr, "Number of equivalent flows:");
 		
 		//hard code of the expected array after sorting in DM order
 		ArrayList<String> expectedOrder = 
 				new ArrayList<String>(Arrays.asList("F1","AF1","F2","AF2","F3","F4","AF4","F5","AF5","F6","F7","F8","F9","F10","AF10"));
 		//check if setFlowsInDMorder sorted in the expected order
-		assertEquals(expectedOrder, DMorder, "check if correct order:");
+		assertEquals(expectedOrder, DMorder, "Check if setting in DM order matches the expected array list:");
 	}
 
 	/* ------------------------------
 	 * Begin getFlowNamesInPriorityOrder tests
 	 * ------------------------------ */
 	@Test
-	void testGetFlowNamesInPriorityOrder() {
-		//instantiation of a new WorkLoad object using StressTest.txt as test file
+	void testGetFlowNamesInPriorityOrder_NotEmpty() {
 		WorkLoad wkld = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
 		
 		//Create a test array list and set flowNamesInPriorityOrder with it
@@ -60,6 +59,11 @@ class WorkLoadTest {
 		ArrayList<String> actualNotEmpty = wkld.getFlowNamesInPriorityOrder();
 		//check if the expected array list and array list created from getter method are equal
 		assertEquals(expectedNotEmpty, actualNotEmpty, "Check if getter retrieved the proper Array List:");
+	}
+	
+	@Test
+	void testGetFlowNamesInPriorityOrder_Empty() {
+		WorkLoad wkld = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
 		
 		//Create an empty array list and set flowNamesInPriorityOrder with it
 		ArrayList<String> expectedEmpty = new ArrayList<>();
@@ -67,14 +71,14 @@ class WorkLoadTest {
 		//instantiation of actual array list using the proper getter method
 		ArrayList<String> actualEmpty = wkld.getFlowNamesInPriorityOrder();
 		//check if the expected array list and array list created from getter method are equal
-		assertEquals(expectedEmpty, actualEmpty, "Check if getter retrieved the proper Array List:");
+		assertEquals(expectedEmpty, actualEmpty, "Check if getter retrieved the empty Array List:");
 	}
 	
 	/* ------------------------------
 	 * Begin getNodeChannel tests
 	 * ------------------------------ */
 	@Test
-	void testGetNodeChannel() {
+	void testGetNodeChannel_ValidNode() {
 		//instantiation of a new WorkLoad object using StressTest.txt as test file
 		WorkLoad wkld = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
 		
@@ -82,18 +86,18 @@ class WorkLoadTest {
 		int actualWithoutSet = wkld.getNodeChannel("A");
 		assertEquals(0, actualWithoutSet, "Node channel prior to setting:");
 		
-		//check to see if getter retrieves correct new value
+		//check to see if getter retrieves correct new value after setting
 		wkld.setNodeChannel("A", 44);
 		int actualWithSet = wkld.getNodeChannel("A");
 		assertEquals(44, actualWithSet, "Node channel after setting:");
 	}
 	
 	@Test
-	public void testGetNodeChannelThrowsNullPointerException() {
+	public void testGetNodeChannel_InvalidNode() {
 	    assertThrows(NullPointerException.class,
 	            ()->{
 	            	WorkLoad wkld = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
-	            	//if given an empty string, should throw NullPointerException
+	            	//if given an input that isn't a valid node, should throw NullPointerException
 	            	wkld.getNodeChannel("");
 	            });
 	}
@@ -102,7 +106,7 @@ class WorkLoadTest {
 	 * Begin getNodesInFlow tests
 	 * ------------------------------ */
 	@Test
-	void testGetNodesInFlow() {
+	void testGetNodesInFlow_ValidFlow() {
 		WorkLoad wkld = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
 		
 		//example case of getNodesInFlow with a correct flow name
@@ -112,6 +116,11 @@ class WorkLoadTest {
 		String expected = "[A, B, C, D, E, J, K, L]";
 		//Test to see if we got the node names in the same order as they exist in the flow specification
 		assertEquals(expected, actualWithCorrectInput, "Order of nodes of flow F9 in StressTest.txt:");
+	}
+	
+	@Test
+	void testGetNodesInFlow_InvalidFlow() {
+		WorkLoad wkld = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
 		
 		//example case of getNodesInFlow with incorrect flow name
 		String[] actual2 = wkld.getNodesInFlow("test");
@@ -119,14 +128,14 @@ class WorkLoadTest {
 		//hard code of the expected string
 		String expectedEmpty = "[]";
 		//Test to see if we correctly went to the else case in getNodesInFlow
-		assertEquals(expectedEmpty, actualWithIncorrectInput, "Check if else case correctly implemented:");
+		assertEquals(expectedEmpty, actualWithIncorrectInput, "Check if using a non-valid flow name correctly returns empty array:");
 	}
 	
 	/* ------------------------------
 	 * Begin setFlowNamesInOriginalOrder tests
 	 * ------------------------------ */
 	@Test
-	void testSetFlowNamesInOriginalOrder() {
+	void testSetFlowNamesInOriginalOrder_NotEmpty() {
 		WorkLoad wkld = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
 		
 		//Create a test array list for setFlowNamesInOriginalOrder. Test array list is arbitrary
@@ -136,6 +145,11 @@ class WorkLoadTest {
 		ArrayList<String> actualNotEmpty = wkld.getFlowNamesInOriginalOrder();
 		//check if the expected array list and array list created from setter method are equal
 		assertEquals(expectedNotEmpty, actualNotEmpty, "Check if setter created the proper Array List:");
+	}
+	
+	@Test
+	void testSetFlowNamesInOriginalOrder_Empty() {
+		WorkLoad wkld = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
 		
 		//Create an empty array list for setFlowNamesInOriginalOrder
 		ArrayList<String> expectedEmpty = new ArrayList<>();
@@ -143,8 +157,9 @@ class WorkLoadTest {
 		//instantiation of actual array list using the proper getter method
 		ArrayList<String> actualEmpty = wkld.getFlowNamesInOriginalOrder();
 		//check if the expected array list and array list created from getter method are equal
-		assertEquals(expectedEmpty, actualEmpty, "Check if setter created the proper Array List:");
+		assertEquals(expectedEmpty, actualEmpty, "Check if setter created the empty Array List:");
 	}
+		
 	
 	/* ------------------------------
 	 * Begin setFlowsInPriorityOrder tests
@@ -179,87 +194,108 @@ class WorkLoadTest {
 		ArrayList<String> expectedOrder = 
 				new ArrayList<String>(Arrays.asList("F1","AF1","F2","AF2","F3","F4","AF4","F5","AF5","F6","F7","F8","F9","F10","AF10"));
 		//check if setFlowsInDMorder sorted in the expected order
-		assertEquals(expectedOrder, priorityOrder, "check if correct order:");
+		assertEquals(expectedOrder, priorityOrder, "Check if setting in Priority order matches the expected array list:");
 	}
 	
 	/* ------------------------------
 	 * Begin getMinPeriod tests
 	 * ------------------------------ */
 	@Test
-	void testGetMinPeriod() {
+	void testGetMinPeriod_Unique() {
 		WorkLoad wkld = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
 		//The expected minimum period in StreesTest.txt is 20
 		int testPeriod = wkld.getMinPeriod();
-		assertEquals(20, testPeriod, "minimum period gotten from StressTest.txt:");
-		
+		assertEquals(20, testPeriod, "Minimum period gotten from StressTest.txt:");
+		}
+	
+	@Test
+	void testGetMinPeriod_Default() {
 		WorkLoad wkld2 = new WorkLoad(1, 0.9, 0.99, "Example.txt");
 		//This is a check for the default period which is 100
 		int testPeriodDefault = wkld2.getMinPeriod();
-		assertEquals(100, testPeriodDefault, "minimum period gotten from Example.txt:");
-		}
+		assertEquals(100, testPeriodDefault, "Minimum period gotten from Example.txt:");
+	}
 	
 	/* ------------------------------
 	 * Begin maxFlowLength tests
 	 * ------------------------------ */
 	@Test
-	void testMaxFlowLength() {
+	void testMaxFlowLength_StressTest() {
 		//Test of fairly average case with StressTest.txt
-		WorkLoad wkld1 = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
-		int actual1 = wkld1.maxFlowLength();
-		assertEquals(8, actual1, "Max flow length for StressTest.txt");
-		
+		WorkLoad wkld = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
+		int actual = wkld.maxFlowLength();
+		assertEquals(8, actual, "Max flow length for StressTest.txt");
+		}
+	
+	@Test
+	void testMaxFlowLength_WARPMIXSchedule0WarpInput() {
 		//Test of different node flow representation case with WARP_MIX_Schedule0-WarpInput.txt
 		WorkLoad wkld2 = new WorkLoad(1, 0.9, 0.99, "WARP_MIX_Schedule0-WarpInput.txt");
 		int actual2 = wkld2.maxFlowLength();
-		assertEquals(5, actual2, "Max flow length for WARP_MIX_Schedule0-WarpInput.txt");		
-		
+		assertEquals(5, actual2, "Max flow length for WARP_MIX_Schedule0-WarpInput.txt");
+	}
+	
+	@Test
+	void testMaxFlowLength_LongChain() {
 		//Test of variable spacing and long flow length case with LongChain.txt
-		WorkLoad wkld3 = new WorkLoad(1, 0.9, 0.99, "LongChain.txt");
-		int actual3 = wkld3.maxFlowLength();
-		assertEquals(26, actual3, "Max flow length for LongChain.txt");
-		}
+		WorkLoad wkld2 = new WorkLoad(1, 0.9, 0.99, "LongChain.txt");
+		int actual2 = wkld2.maxFlowLength();
+		assertEquals(26, actual2, "Max flow length for LongChain.txt");
+	}
 	
 	/* ------------------------------
 	 * Begin getFlowDeadline tests
 	 * ------------------------------ */
 	@Test
-	void testGetFlowDeadline() {
+	void testGetFlowDeadline_Unique() {
 		WorkLoad wkld = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
 		//The expected deadline of flow F9 in StressTest.txt is 100
 		int testDeadline = wkld.getFlowDeadline("F9");
 		assertEquals(100, testDeadline, "deadline gotten for flow F9 in StressTest.txt:");
-		
+		}
+	
+	@Test
+	void testGetFlowDeadline_Default() {
 		WorkLoad wkld2 = new WorkLoad(1, 0.9, 0.99, "Example.txt");
 		//This is a check for the default deadline which is 100
 		int testDeadlineDefault = wkld2.getFlowDeadline("F0");
 		assertEquals(100, testDeadlineDefault, "deadline gotten for flow F0 in Example.txt:");
-		
+	}
+	
+	@Test
+	void testGetFlowDeadline_InvalidInput() {
 		WorkLoad wkld3 = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
 		//Test the "Bad situation" where the flow name given does not exist
 		//Should give the default of 100
 		int testDeadlineFlowNameDNE = wkld3.getFlowDeadline("");
 		assertEquals(100, testDeadlineFlowNameDNE, "check for a flow name that does not exist:");
-		}
+	}
 	
 	/* ------------------------------
 	 * Begin getNodeNamesOrderedAlphabetically tests
 	 * ------------------------------ */
 	@Test
-	void testGetNodeNamesOrderedAlphabetically() {
+	void testGetNodeNamesOrderedAlphabetically_AllUpperCase() {
 		//check a fairly average case with StressTest.txt
 		WorkLoad wkld1 = new WorkLoad(1, 0.9, 0.99, "StressTest.txt");
 		String[] alphaOrder1 = wkld1.getNodeNamesOrderedAlphabetically();
 		String alphaOrderActual1 = Arrays.toString(alphaOrder1); 
 		String expected1 = "[A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, Y]";
 		assertEquals(expected1, alphaOrderActual1, "check if the node names in StressTest.txt were ordered alphabetically:");
-		
+	}
+	
+	@Test
+	void testGetNodeNamesOrderedAlphabetically_UpperAndLowerCase() {
 		//check a slightly more complex case involving a lower case letter with LongChain.txt
 		WorkLoad wkld2 = new WorkLoad(1, 0.9, 0.99, "LongChain.txt");
 		String[] alphaOrder2 = wkld2.getNodeNamesOrderedAlphabetically();
 		String alphaOrderActual2 = Arrays.toString(alphaOrder2);
 		String expected2 = "[A, B, C, D, E, F, G, H, I, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z, a]";
 		assertEquals(expected2, alphaOrderActual2, "check if the node names in LongChain.txt were ordered alphabetically:");
-		
+	}
+	
+	@Test
+	void testGetNodeNamesOrderedAlphabetically_AllNumbers() {
 		//check a numerical case with WARP_MIX_Schedule0-WarpInput.txt
 		WorkLoad wkld3 = new WorkLoad(1, 0.9, 0.99, "WARP_MIX_Schedule0-WarpInput.txt");
 		String[] alphaOrder3 = wkld3.getNodeNamesOrderedAlphabetically();
@@ -268,7 +304,10 @@ class WorkLoadTest {
 				+ "38, 40, 41, 42, 48, 52, 53, 54, 55, 56, 58, 59, 64, 66, 68, 71, 72, 73, 74, 76, 80, 82, 83, 84]";
 		assertEquals(expected3, alphaOrderActual3, 
 				"check if the node names in WARP_MIX_Schedule0-WarpInput.txt were ordered alphabetically (numerically):");
-		
+	}
+	
+	@Test
+	void testGetNodeNamesOrderedAlphabetically_LettersAndNumbers() {
 		//check a case where node names consist of both letter and numbers with SeeSpray.txt
 		WorkLoad wkld4 = new WorkLoad(1, 0.9, 0.99, "SeeSpray.txt");
 		String[] alphaOrder4 = wkld4.getNodeNamesOrderedAlphabetically();
@@ -277,5 +316,4 @@ class WorkLoadTest {
 				+ "C22, C23, C24, C25, C26, C27, C28, C29, C3, C30, C4, C5, C6, C7, C8, C9]";
 		assertEquals(expected4, alphaOrderActual4, "check if the node names in SeeSpray.txt were ordered alphabetically:");
 	}
-
 }
