53dea87c55071de208963fff58a39ebb1704e547
dlin4 <dlin4@uiowa.edu>
2022-11-18 15:18:41 -0600

    Added Timeout and 2 tests

--- src/edu/uiowa/cs/warp/ChannelVisualizationTest.java
+++ src/edu/uiowa/cs/warp/ChannelVisualizationTest.java
@@ -3,10 +3,13 @@ package edu.uiowa.cs.warp;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import java.util.concurrent.TimeUnit;
 
 import edu.uiowa.cs.warp.SystemAttributes.ScheduleChoices;
 
 class ChannelVisualizationTest {
+
 	
 	//NOTE: Until ChannelAnalysis.java is fully implemented, filled tables with flow instructions cannot be tested.
 
@@ -41,10 +44,20 @@ class ChannelVisualizationTest {
 		ChannelVisualization channelVis = new ChannelVisualization(warp);
 		return channelVis;
 	}
-	
+	//Helper method that creates a ChannelVisualization object to be tested according to an input file and other WarpSystem parameters.
+	//MODIFIED ChannelAnalysis class (see classes below) to test table with cells fully filled.
+	//@author lldeng, dlin4
+	private ChannelVisualization createChannelVisualizationModFullyFilled(Integer numFaults, String inputFile, Integer numChannels) {
+		WorkLoad workload = new WorkLoad(numFaults, .9, .99, inputFile);
+		ScheduleChoices choice = ScheduleChoices.PRIORITY;
+		WarpInterface warp = new WarpSystemModFullyFilled(workload, numChannels, choice);
+		ChannelVisualization channelVis = new ChannelVisualization(warp);
+		return channelVis;
+	}
 	//Tests that the header prints out additional information (other parameters) when numFaults is set to 0
 	//@author lldeng
 	@Test
+	@Timeout(value = 1, unit = TimeUnit.SECONDS)
 	void createHeaderTestNumFaultsEqualsZero() {
 		ChannelVisualization channelVis = createChannelVisualization(0, "ExampleX.txt", 16);
 		Description actual = channelVis.createHeader();
@@ -64,6 +77,7 @@ class ChannelVisualizationTest {
 	//Tests that the header created by createHeader() matches the expected header of ExampleX.txt with numFaults 1
 	//@author dlin4
 	@Test
+	@Timeout(value = 1, unit = TimeUnit.SECONDS)
 	void createHeaderTestExampleX() {
 		ChannelVisualization channelVis = createChannelVisualization(1, "ExampleX.txt", 16);
 		Description actual = channelVis.createHeader();
@@ -81,6 +95,7 @@ class ChannelVisualizationTest {
 	//Tests that the header created by createHeader() matches the expected header of Example.txt with numFaults 2
 	//@author lldeng
 	@Test
+	@Timeout(value = 1, unit = TimeUnit.SECONDS)
 	void createHeaderTestExample() {
 		ChannelVisualization channelVis = createChannelVisualization(2, "Example.txt", 16);
 		Description actual = channelVis.createHeader();
@@ -98,6 +113,7 @@ class ChannelVisualizationTest {
 	//Tests that an empty visualization table is correctly initialized for ExampleX.txt
 	//@author dlin4
 	@Test
+	@Timeout(value = 1, unit = TimeUnit.SECONDS)
 	void visualizationTestEmptyTableExampleX() {
 		ChannelVisualization channelVis = createChannelVisualization(1, "ExampleX.txt", 16);
 		Description actual = channelVis.visualization();
@@ -116,6 +132,7 @@ class ChannelVisualizationTest {
 	//Tests that an empty visualization table is correctly initialized for TestBug.txt
 	//@author lldeng
 	@Test
+	@Timeout(value = 1, unit = TimeUnit.SECONDS)
 	void visualizationTestEmptyTableTestBug() {
 		ChannelVisualization channelVis = createChannelVisualization(1, "TestBug.txt", 16);
 		Description actual = channelVis.visualization();
@@ -134,6 +151,7 @@ class ChannelVisualizationTest {
 	//Tests that an empty table with only one row is correctly initialized for TestBug.txt with numChannels = 1
 	//@author lldeng
 	@Test
+	@Timeout(value = 1, unit = TimeUnit.SECONDS)
 	void visualizationTestOneChannelTestBug() {
 		ChannelVisualization channelVis = createChannelVisualization(1, "TestBug.txt", 1);
 		Description actual = channelVis.visualization();
@@ -152,6 +170,7 @@ class ChannelVisualizationTest {
 	//Tests that an empty table with 25 rows is correctly initialized for ExampleX.txt with numChannels = 25
 	//@author lldeng
 	@Test
+	@Timeout(value = 1, unit = TimeUnit.SECONDS)
 	void visualizationTestTwentyFiveChannelsExampleX() {
 		ChannelVisualization channelVis = createChannelVisualization(1, "ExampleX.txt", 25);
 		Description actual = channelVis.visualization();
@@ -170,6 +189,7 @@ class ChannelVisualizationTest {
 	//Tests that a table with no rows or columns is made using the modded ChannelAnalysis class(Test file is irrelevant)
 	//@author lldeng
 	@Test
+	@Timeout(value = 1, unit = TimeUnit.SECONDS)
 	void visualizationTestNoTable() {
 		ChannelVisualization channelVis = createChannelVisualizationModNone(1, "ExampleX.txt", 16);
 		Description actual = channelVis.visualization();
@@ -185,6 +205,7 @@ class ChannelVisualizationTest {
 	//Tests that a table with actual input string values is created using the modded ChannelAnalysis class
 	//@author lldeng
 	@Test
+	@Timeout(value = 1, unit = TimeUnit.SECONDS)
 	void visualizationTestPartiallyFilledTable() {
 		ChannelVisualization channelVis = createChannelVisualizationModFilled(1, "ExampleX.txt", 16);
 		Description actual = channelVis.visualization();
@@ -201,6 +222,40 @@ class ChannelVisualizationTest {
 		//System.out.print(actual);
 		//System.out.print(expected);
 		assertTrue(expected.equals(actual));
+	}
+	//Tests that a full table with actual input string values is created using the modded ChannelAnalysis class
+	//@author dlin4
+	@Test
+	@Timeout(value = 1, unit = TimeUnit.SECONDS)
+	void visualizationTestFullFilledTable() {
+		ChannelVisualization channelVis = createChannelVisualizationModFullyFilled(1, "ExampleX.txt", 16);
+		Description actual = channelVis.visualization();
+		
+		Description expected = new Description();
+		expected.add("Channel/Time Slot 0	1	2	3	4	5	6	7	8	9	\n");
+		for(int i = 0; i < 16; i++) {
+			expected.add(String.format("%s	test	test	test	test	test	test	test	test	test	test\n", i));
+		}
+		//System.out.print(actual);
+		//System.out.print(expected);
+		assertTrue(expected.equals(actual));
+	}
+	//Tests that an empty table has the correct row index when using the .get() method
+	//@author dlin4
+	@Test
+	@Timeout(value = 1, unit = TimeUnit.SECONDS)
+	void visualizationTestTableExampleXGetRowIndex() {
+		ChannelVisualization channelVis = createChannelVisualization(1, "ExampleX.txt", 16);
+		Description actual = channelVis.visualization();
+
+		Description expected = new Description();
+		expected.add("Channel/Time Slot 0	1	2	3	4	5	6	7	8	9	\n");
+		for(int i = 0; i < 5; i++) {
+			expected.add(String.format("%s	-	-	-	-	-	-	-	-	-	-\n", i));
+		}
+		//System.out.print(actual.get(3));
+		//System.out.print(expected.get(3));
+		assertTrue(expected.get(3).equals(actual.get(3)));
 	}
 	
 	/* Currently not in use. Will be uncommented when ChannelAnalysis is fully implemented.
@@ -244,6 +299,9 @@ class ChannelVisualizationTest {
 		assertTrue(expected.equals(actual));
 	}*/
 }
+
+//Tests ends here//
+
 
 //This class extends the ChannelAnalysis class to override the current implementation in order to test that
 //ChannelVisualization correctly creates a visualization with no rows or columns when the ChannelAnalysis
@@ -320,10 +378,51 @@ class WarpSystemModFilled extends WarpSystem {
 		super(workLoad, numChannels, choice);
 		// TODO Auto-generated constructor stub
 	}
-	
 	@Override
 	public ChannelAnalysis toChannelAnalysis() {
 		ChannelAnalysis ca = new ChannelAnalysisModFilled(this);
 		return ca;
 	}
-}
+}	
+//This class extends the ChannelAnalysis class to override the current implementation in order to test that
+//ChannelVisualization correctly creates a visualization with actual string values when the ChannelAnalysis
+//table has those string values set in all Channel/Timeslot cells.
+//@author lldeng, dlin4
+class ChannelAnalysisModFullyFilled extends ChannelAnalysis {
+		
+		ChannelAnalysisModFullyFilled(WarpInterface warp) {
+			this(warp.toProgram());
+		}
+
+		ChannelAnalysisModFullyFilled(Program program) {
+			super(program);
+			buildFullyFilledChannelAnalysisTable();
+		}
+		
+		private void buildFullyFilledChannelAnalysisTable() {
+			var numColumns = programTable.getNumRows();
+			var numRows = program.getNumChannels();
+			channelAnalysisTable = new ProgramSchedule(numRows,numColumns);
+			for(int i = 0; i < numRows; i++) {
+				for(int j = 0; j < numColumns; j++) {
+					channelAnalysisTable.set(i, j, "test");
+				}
+			}
+		}
+	}
+//This class extends the WarpSystem class to create a WarpSystem object using the modified ChannelAnalysis
+//class that creates a table with input strings at all table positions (see class directly above)
+//@author lldeng, dlin4
+class WarpSystemModFullyFilled extends WarpSystem {
+
+		public WarpSystemModFullyFilled(WorkLoad workLoad, Integer numChannels, ScheduleChoices choice) {
+			super(workLoad, numChannels, choice);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		public ChannelAnalysis toChannelAnalysis() {
+			ChannelAnalysis ca = new ChannelAnalysisModFullyFilled(this);
+			return ca;
+		}
+	}
