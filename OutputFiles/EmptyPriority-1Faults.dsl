a006a5fc0c432e4912d4fa82b58026eff3c3fe84
lldeng <lldeng@uiowa.edu>
2022-12-05 16:50:49 -0600

    Started ChannelAnalysis Tests

---
+++ OutputFiles/EmptyPriority-1Faults.dsl
@@ -0,0 +1,11 @@
+// Course CS2820 Authentication Tag: tLLmtxnPY8e4kAlUWVjtQn8OXENZwfc5+k1lPlrhhKhmL0BvJqU3xXhzxBUb/rebcwvmBIz0ZqtUl8v8yyvOKOElCZ0afubYj/JS9x1Btz8=
+WARP system for graph Empty created with the following parameters:
+Scheduler Name:	Priority
+numFaults:	1
+Time Slot	A	B
+0	if has(F0) push(F0: A -> B, #1)	wait(#1)
+1	if has(F0) push(F0: A -> B, #2)	wait(#2)
+2	sleep	sleep
+3	sleep	sleep
+4	sleep	sleep
+// All flows meet their deadlines
--- src/edu/uiowa/cs/warp/ChannelAnalysisTest.java
+++ src/edu/uiowa/cs/warp/ChannelAnalysisTest.java
@@ -4,11 +4,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.uiowa.cs.warp.SystemAttributes.ScheduleChoices;
+
 class ChannelAnalysisTest {
 
+	private ChannelAnalysis createChannelAnalysis(Integer numFaults, String inputFile, Integer numChannels) {
+		WorkLoad workload = new WorkLoad(numFaults, .9, .99, inputFile);
+		ScheduleChoices choice = ScheduleChoices.PRIORITY;
+		WarpInterface warp = new WarpSystem(workload, numChannels, choice);
+		ChannelAnalysis channelAnalysis = new ChannelAnalysis(warp);
+		return channelAnalysis;
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void buildChannelAnalysisTableTestEmpty() {
+		ChannelAnalysis channelAnalysis = createChannelAnalysis(1, "Empty.txt", 16);
+		ProgramSchedule expected = new ProgramSchedule(16, 5);
+		expected.set(1,  0, "[A]::F0:(A:B)");
+		expected.set(2, 1, "[A]::F0:(A:B)");
+		ProgramSchedule actual = channelAnalysis.getChannelAnalysisTable();
+		assertEquals(actual, expected);
+		System.out.println(actual.toString());
+		System.out.println(expected.toString());
 	}
 
 }
